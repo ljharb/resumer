848b8755f5d89c7287e48d98b6f140553afbaf1e
James Halliday <mail@substack.net>
2013-05-17 14:31:14 -0700

    ripped out of bashful

---
+++ index.js
@@ -0,0 +1,28 @@
+var nextTick = typeof setImmediate !== 'undefined'
+    ? setImmediate
+    : process.nextTick
+;
+
+module.exports = function () {
+    var tr = through();
+    tr.pause();
+    var resume = tr.resume;
+    var pause = tr.pause;
+    var paused = false;
+    
+    tr.pause = function () {
+        paused = true;
+        return pause.apply(this, arguments);
+    };
+    
+    tr.resume = function () {
+        paused = false;
+        return resume.apply(this, arguments);
+    };
+    
+    nextTick(function () {
+        if (!paused) tr.resume();
+    });
+    
+    return tr;
+};
