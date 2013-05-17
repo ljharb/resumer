7f52e27a8b24c9223a8a1a55a8cb887b36f08f9c
James Halliday <mail@substack.net>
2013-05-17 15:02:02 -0700

    pause/resume test

--- test/resume.js
+++ test/resume.js
@@ -11,8 +11,27 @@ test('implicit resume', function (t) {
     }));
 });
 
+test('pause/resume', function (t) {
+    t.plan(2);
+    
+    var s = createStream();
+    s.pause();
+    
+    var paused = true;
+    setTimeout(function () {
+        paused = false;
+        s.resume();
+    }, 100);
+    
+    s.pipe(concat(function (err, body) {
+        t.equal(paused, false);
+        t.equal(body, 'beep boop\n');
+    }));
+});
+
 function createStream () {
     var stream = resumer();
     stream.queue('beep boop\n');
+    stream.queue(null);
     return stream;
 }
