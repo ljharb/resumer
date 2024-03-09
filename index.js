f43f860083eb3472a00dd470c0f2afecfc22f356
Jordan Harband <ljharb@gmail.com>
2024-03-08 22:40:20 -0800

    [Robustness] use `call-bind` rather than `.apply`

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ var nextTick = typeof setImmediate === 'undefined'
 	? process.nextTick
 	: setImmediate;
 
+var callBind = require('call-bind');
+
 /** @type {import('.')} */
 module.exports = function resumer(write, end) {
 	var tr = through(write, end);
 	tr.pause();
-	var resume = tr.resume;
-	var pause = tr.pause;
+	var resume = callBind.apply(tr.resume);
+	var pause = callBind.apply(tr.pause);
 	var paused = false;
 
 	tr.pause = function () {
 		paused = true;
 		// @ts-expect-error https://github.com/microsoft/TypeScript/issues/57164
-		return pause.apply(this, arguments);
+		return pause(this, arguments);
 	};
 
 	tr.resume = function () {
 		paused = false;
 		// @ts-expect-error https://github.com/microsoft/TypeScript/issues/57164
-		return resume.apply(this, arguments);
+		return resume(this, arguments);
 	};
 
 	nextTick(function () {
